8cdd4ea9261c7da22de46e3a31eafc63d2447e48
samyakvaidya10 <samyakmcv@gmail.com>
2022-10-05 12:18:37 +0530

    improved player movement logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,34 +4,48 @@ export default function Board(){
     
     const row=25;
     const n=5;
-    const start=(n*(n-1));
-    //const [boardArr,setBoardArr]=React.useState(()=>createBoard())
+
+    
+    const [boardMap,setBoardMap]=React.useState(()=>createBoardMap())
     const [boardArr,setBoardArr]=React.useState(()=>createBoard())
     const [player,setPlayer]=React.useState(0);
     
+    
     function createBoard(){
         let boardArr=[]
-        for(let i=n-1;i>=0;i--){                        //Create array having values for 5x5 board 
-            if((i&1)===0){                              //    20, 21, 22, 23, 24, 
-               for(let j=(n*i);j<(n*i)+5;j++){          //    19, 18, 17, 16, 15,  
-                boardArr.push(j);                       //    10, 11, 12, 13, 14      
-               }                                        //    9,  8,  7,  6,  5,           
-            }else{                                      //    0,  1,  2,  3,  4,
-              for(let j=(n*(i+1)-1);j>=(n*i);j--){        
-                boardArr.push(j);
-               }
-            }
-          }
-
+        for(let i=0;i>n*n;i++){                        
+            boardArr.push("");
+        }
         return boardArr;
     }
+
+    function createBoardMap(){
+        const myMap=new Map();
+        let c=0;
+        for(let i=n-1;i>=0;i--){                        //Map array index with board value for 5x5 board 
+            if((i&1)===0){                                  //    20, 21, 22, 23, 24, 
+                for(let j=(n*i);j<(n*i)+5;j++){             //    19, 18, 17, 16, 15,  
+                    myMap.set(c,j);                         //    10, 11, 12, 13, 14  
+                    c++;                                    //    9,  8,  7,  6,  5,  
+                }                                           //    0,  1,  2,  3,  4,         
+            }else{                                      
+                for(let j=(n*(i+1)-1);j>=(n*i);j--){        
+                    myMap.set(c,j);
+                    c++;                            
+                }
+            }
+        }
+        
+        return myMap;
+    }
+
     //to change board val to display player location
     React.useEffect(function(){
-        let playerIndex=boardArr.indexOf(player);
+        let playerIndex=boardMap.get(player);
         setBoardArr(function(prevState){
             let newState=[...prevState];
-            newState[playerIndex]="sam"
-            newState[playerIndex-1]=player-1;
+            newState[playerIndex]="⦿"
+            newState[boardMap.get(player-1)]="";
             return newState;
         })
     },[player])
@@ -43,7 +57,7 @@ export default function Board(){
 
     let board=[]
     for(let i=0;i<row;i++){
-            board.push(<div className="square"><p>{boardArr[i]}</p></div>)
+            board.push(<div key={i} className="square"><h1>{boardArr[i]}</h1></div>)
     }
 
 
